1556ba4eea075a9dd340400cfaa0db3729e1c72f
Don Hardman <don.hardman@muvon.io>
2024-02-27 12:13:56 +0700

    Adjust logic of progress bar and not refresh the page to let user interract with search

--- app/client/component/progress-bar/index.js
+++ app/client/component/progress-bar/index.js
@@ -3,58 +3,64 @@ import nav from 'lib/navigation'
 
 const pollDuration = 1000
 export default element => {
-  const d = domd(element)
-  const api_url = element.getAttribute('data-api-url')
+	const d = domd(element)
+	const api_url = element.getAttribute('data-api-url')
 
-  let pollingInterval = null
-  let percentage = 0
-  let step = 0
-  let duration = 1
-  const updateProgressBar = (percentage, duration) => {
-  	element.style.setProperty('--progress-duration', `${duration}s`)
-  	element.style.setProperty('--progress-value', (percentage + step) / 100)
-  }
+	let pollingInterval = null
+	let percentage = 0
+	let step = 0
+	let duration = 1
+	const updateProgressBar = (percentage, duration) => {
+		element.style.setProperty('--progress-duration', `${duration}s`)
+		element.style.setProperty('--progress-value', (percentage + step) / 100)
+	}
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(api_url)
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      const [err, result] = await response.json()
-      if (!step) {
-      	step = 1 / (parseInt(result.expected_issues / 100, 10) + 1)
-      } else {
-      	duration = parseInt(Math.min(100, result.expected_issues) / 6, 10)
-      }
-      if (result.indexed_percentage > percentage) {
-      	nav.load(location.href)
-      }
-      percentage = result.indexed_percentage
-      updateProgressBar(percentage, duration)
-      if (result.is_indexing) {
-      	if (result.issues === 0 && result.comments === 0) {
-      		element.classList.add('show-preload')
-      	}
+	const fetchData = async () => {
+		try {
+			const response = await fetch(api_url)
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`)
+			}
+			const [err, result] = await response.json()
+			if (!step) {
+				step = 1 / (parseInt(result.expected_issues / 100, 10) + 1)
+			} else {
+				duration = parseInt(Math.min(100, result.expected_issues) / 6, 10)
+			}
 
-      	element.classList.add('progress-bar')
-      } else {
-    		element.classList.remove('progress-bar')
-    		element.classList.remove('show-preload')
-    	  clearInterval(pollingInterval)
-    	  console.log('Indexing complete, stopped polling.')
-      }
-    } catch (error) {
-      console.error('Failed to fetch data:', error)
-    }
-  }
+			percentage = result.indexed_percentage
+			updateProgressBar(percentage, duration)
+			const items = result.issues + result.comments + result.pull_requests
+			const showPreload = items === 0
 
-  // Start polling
-  fetchData()
-  pollingInterval = setInterval(fetchData, pollDuration)
+			// Refresh the page only when we lock it otherwise user may
+			if (!showPreload && result.indexed_percentage > percentage) {
+				nav.load(location.href)
+			}
+			if (result.is_indexing) {
+				// Show preloader screen when we just starting without data before
+				if (showPreload) {
+					element.classList.add('show-preload')
+				}
 
-  // Return a function to allow manual stopping of the polling
-  return () => {
-    clearInterval(pollingInterval)
-  }
+				element.classList.add('progress-bar')
+			} else {
+				element.classList.remove('progress-bar')
+				element.classList.remove('show-preload')
+				clearInterval(pollingInterval)
+				console.log('Indexing complete, stopped polling.')
+			}
+		} catch (error) {
+			console.error('Failed to fetch data:', error)
+		}
+	}
+
+	// Start polling
+	fetchData()
+	pollingInterval = setInterval(fetchData, pollDuration)
+
+	// Return a function to allow manual stopping of the polling
+	return () => {
+		clearInterval(pollingInterval)
+	}
 }
